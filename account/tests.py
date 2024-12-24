7f3a073414a5aa7dc3b279335bdf2edff983de09
messi <messi10@.fcb>
2024-12-24 20:53:52 +0000

    testing fonction

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from .forms import LoginForm
+#from django import forms
 
-# Create your tests here.
+class LoginForTest(TestCase):
+    def test_loginform(self):
+        
+        #donnée valide pour le formulaire
+        form_data = {
+            "user_name" : "name_test",
+            "pass_word" : "password_test"
+        }
+
+        form = LoginForm(data=form_data)
+
+        #verification de la validation du formulaire
+        self.assertTrue(form.is_valid())
+
+    
+
+    def signup(self):
+       pass
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Article(models.Model):
   title = models.CharField(max_length=100)
   summary = models.TextField(max_length=500) 
