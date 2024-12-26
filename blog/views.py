5c4abae7bea7ea677fbd8ee98f4d2fe3e561a878
contact@thierdl.sn <contact@thierdl.sn>
2024-12-26 16:09:22 +0000

    modify

--- blog/views.py
+++ blog/views.py
@@ -13,7 +13,7 @@ def index_views(request):
 
 
 def list_views(request):
-  articles = Article.objects.all().order_by('-date_create')
+  articles = Article.objects.all().order_by('-date_create') # order_by('- or +') trier par le plus recent(date de creation)
   paginator = Paginator(articles, 9)
   page_number = request.GET.get('page')
   page_obj = paginator.get_page(page_number)
--- projet_blog/settings.py
+++ projet_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7aud_8pft5r%*js0b6!z%22*3yga^yruol%7@l^vd5l_s)wp(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','projet-blog.onrender.com'] #https://projet-blog.onrender.com
 
